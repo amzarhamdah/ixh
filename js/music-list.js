3827f4229cc69a2067d1cbf81e7728f8eb62c018
amzarhamdah <amzarhamdah0@gmail.com>
2024-10-08 16:56:33 +0700

    first

---
+++ js/music-list.js
@@ -0,0 +1,140 @@
+
+let allMusic = [
+
+  {
+
+    name: "Beautiful in white",
+
+    artist: "shane filan",
+
+    img: "music-0",
+
+    src: "music-0"
+
+  },
+  
+  {
+
+    name: "Old Love",
+
+    artist: "yuji,putri dahlia",
+
+    img: "music-1",
+
+    src: "music-1"
+
+  },
+
+  {
+
+    name: "Eyes Blue X Terpikat Senyummu",
+
+    artist: "Stromxx",
+
+    img: "music-2",
+
+    src: "music-2"
+
+  },
+
+  {
+
+    name: "Losing Us",
+
+    artist: "Raissa Anggiani",
+
+    img: "music-3",
+
+    src: "music-3"
+
+  },
+
+  {
+
+    name: "Locked out of Heaven",
+
+    artist: "Bruno Mars",
+
+    img: "music-4",
+
+    src: "music-4"
+
+  },
+
+  {
+
+    name: "Love Story",
+
+    artist: "Taylor Swift",
+
+    img: "music-5",
+
+    src: "music-5"
+
+  },
+
+  {
+
+    name: "Menemukanmu",
+
+    artist: "Seventeen",
+
+    img: "music-6",
+
+    src: "music-6"
+
+  },
+
+  {
+
+    name: "Rewrite The Stars",
+
+    artist: "James Arthur,Anne-Marry",
+
+    img: "music-7",
+
+    src: "music-7"
+
+  },
+
+  {
+
+    name: "Jodoh Pasti Bertemu",
+
+    artist: "Afgan",
+
+    img: "music-8",
+
+    src: "music-8"
+
+  },
+
+  {
+
+    name: "Perahu Kertas",
+
+    artist: "Maudy Ayunda",
+
+    img: "music-9",
+
+    src: "music-9"
+
+  },
+
+  {
+
+    name: "Saturn",
+
+    artist: "SZA",
+
+    img: "music-10",
+
+    src: "music-10"
+
+}
+// Tambahkan Music di sini
+// add music here
+// Pastikan foto nya jpg 
+// Make sure the Img is jpg 
+
+];
